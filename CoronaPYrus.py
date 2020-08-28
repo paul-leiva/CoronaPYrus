158fa70eedb4b8aacfa328784811751169d55330
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2020-08-28 08:59:01 -0400

    Attempting Scrape

--- CoronaPYrus.py
+++ CoronaPYrus.py
@@ -3,8 +3,16 @@ import requests
 import matplotlib.pyplot as plt
 
 URL = 'https://bing.com/covid'
+WHO = 'https://covid19.who.int/table'
 
-response = requests.get(URL)
+response = requests.get(WHO)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup.title.string)
+#print(soup)
+
+#soup.find_all(class="sc-fzplgP bfDTAo th")
+
+#top_row = soup.find(attrs={"class": "sc-fzplgP bfDTAo th"})
+#top_row = filter(filter_tags, soup.find(attrs={'class': 'sc-fzplgP bfDTAo th'}).find_all(text=True))
+top_row = soup.p['class']
+print(top_row)
