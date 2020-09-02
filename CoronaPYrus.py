1992cfaf6bc96b2af8eecd89ec4a9a0228fe5b3f
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2020-09-02 09:03:29 -0400

    Intermediate update

--- CoronaPYrus.py
+++ CoronaPYrus.py
@@ -1,12 +1,28 @@
+import time
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import matplotlib.pyplot as plt
 
 URL = 'https://bing.com/covid'
 WHO = 'https://covid19.who.int/table'
 
-response = requests.get(WHO)
-soup = BeautifulSoup(response.text, 'html.parser')
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)
+driver.get(URL)
+time.sleep(3)
+page = driver.page_source
+driver.quit()
+soup = BeautifulSoup(page, 'html.parser')
+container = soup.find('div', attrs={
+    'class':'confirmed'})
+print(container)
+
+#response = requests.get(URL)
+#soup = BeautifulSoup(response.text, 'html.parser')
 
 #print(soup)
 
@@ -14,5 +30,8 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 #top_row = soup.find(attrs={"class": "sc-fzplgP bfDTAo th"})
 #top_row = filter(filter_tags, soup.find(attrs={'class': 'sc-fzplgP bfDTAo th'}).find_all(text=True))
-top_row = soup.p['class']
-print(top_row)
+#for div in soup.findAll('div', {'class': 'sc-fzplgP bfDTAo th'}):
+#    print(div.contents[0])
+#print(soup.findAll('div', {'class': 'confirmed'}))
+#print(soup.findAll(class_ = 'areaName'))
+print(soup.title)
